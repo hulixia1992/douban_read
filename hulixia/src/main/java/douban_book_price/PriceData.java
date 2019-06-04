7b7b31472dc458ffb7d6a423ff14c8a8f42c5006
hulixia <hulixia@sm.vvip-u.com>
2019-06-04 15:55:54 +0800

    添加查询价格

---
+++ hulixia/src/main/java/douban_book_price/PriceData.java
@@ -0,0 +1,7 @@
+package douban_book_price;
+
+public class PriceData {
+    //https://www.amazon.cn/s?k=9787562482550&i=stripbooks&__mk_zh_CN=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99&ref=nb_sb_noss
+    public String price;
+    public String ISBN;
+}
---
+++ hulixia/src/main/java/douban_book_price/PriceUtils.java
@@ -0,0 +1,11 @@
+package douban_book_price;
+
+/**
+ * 搜索亚马逊的图书价格
+ */
+public class PriceUtils {
+    public static String USD="USD";//美元
+    public static void searchBook() {
+
+    }
+}
